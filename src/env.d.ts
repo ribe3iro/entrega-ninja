f14a91f1e86780fbe75c8c854e2925255685d5bf
Joao Pedro <joaopedrorib@hotmail.com>
2023-02-10 17:14:57 -0300

    first commit

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    TOKEN: string;
+    CLIENT_ID: string;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { REST, Routes, ActivityType, Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!'
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('ready', () => {
+  console.log(`Bot is currently online`);
+  client.user?.setPresence({
+    status: "online",
+    activities: [
+      {
+        type: ActivityType.Listening,
+        name: "/help",
+      },
+    ],
+  });
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
